c52f876500562a2668d410dacd27e659abb9be28
Tim Thiel <timthiel@Tims-MBP.fritz.box>
2023-12-02 13:45:33 +0100

    new Domain

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,21 +13,22 @@ const Home = () => {
       <section className='w-full md:pt-[100px] md:pb-[70px] pt-[130px] pb-20 mb-10 dark:bg-slate-800 bg-slate-200'>
         <div className="container text-center px-3">
           <Text title className='text-3xl'>
-            Blog template using Next Js, Typescript and Taildwind CSS
+            Fratt Blog - Learn new
+            <br/>
             <a href='./blog/First-Website.tsx'></a>
-            Hier ist der Link für den ersten Blog Artikel
+
           </Text>
           <div>
-            <h1>Test Test Test Test Test</h1>
+            <h1>This is Fratt</h1>
           </div>
 
           <Text p className="mt-3 text-xl">
-            This is a simple and static component based blog template for everyone.
+            Blog
           </Text>
 
           <div className='flex justify-center mt-5 flex-wrap '>
-            <a href="https://github.com/webexpe13/blog-template-using-nextjs-typescript-tailwindcss" target="_blank" rel="noopener noreferrer" className='flex items-center justify-center rounded-md bg-blue-600 px-4 pb-2 text-white hover:text-white shadow-lg hover:shadow-none transition-all mb-3 md:mx-5 mx-2'>
-              <span className='text-xl pt-2 block'>Github</span>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className='flex items-center justify-center rounded-md bg-blue-600 px-4 pb-2 text-white hover:text-white shadow-lg hover:shadow-none transition-all mb-3 md:mx-5 mx-2'>
+              <span className='text-xl pt-2 block'>Instagram</span>
             </a>
             <LinkTo href="/pages/about-us.tsx" passHref className='flex items-center justify-center rounded-md bg-blue-600 px-4 pb-2 text-white hover:text-white shadow-lg hover:shadow-none transition-all mb-3 md:mx-5 mx-2'>
               <span className='text-xl pt-2 block'>About</span>
